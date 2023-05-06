0e89fd14ea9ebad36757195a79a376fec0e9d15d
mateusz.lewicki <mateusz.lewicki@windowslive.com>
2023-05-07 01:26:52 +0200

    feat: create fragment

--- js/queueItem.js
+++ js/queueItem.js
@@ -3,28 +3,15 @@ export class QueueItem {
       this.image = image;
       this.title = title;
       this.artist = artist;
-/* 
-<div class="next-media w-90 grid grid-2-2">
-                            <div class="icon grid-cs-1 grid-ce-2 grid-rs-1 grid-re-3">
-                                IMAGE
-                            </div>
-                            <div class="title grid-cs-2 grid-ce-3 grid-rs-1 grid-re-2">
-                                RADIOACTIVE
-                            </div>
-                            <div class="artist grid-cs-2 grid-ce-3 grid-rs-2 grid-re-3">
-                                Imaging Dragons
-                            </div>
-                        </div>
-
-    */ }
-      fragment() {
+    }
+    fragment() {
         let fragment = new DocumentFragment();
         
         let mainContainer = document.createElement("div");
-        mainContainer.classList.add(["next-media", "w-90", "grid", "grid-2-2"]);
+        mainContainer.classList.add("next-media", "w-90", "grid", "grid-2-2","mb-15");
 
         let imageContainer = document.createElement("div");
-        imageContainer.classList.add(["icon", "grid-cs-1", "grid-ce-2", "grid-rs-1", "grid-re-3"])
+        imageContainer.classList.add("icon", "grid-cs-1", "grid-ce-2", "grid-rs-1", "grid-re-3")
 
         let image = document.createElement("img");
         image.src = this.image;
@@ -32,28 +19,37 @@ export class QueueItem {
 
 
         let titleContainer = document.createElement("div");
-        titleContainer.classList.add(["title", "grid-cs-2", "grid-ce-3", "grid-rs-1", "grid-re-2"])
+        titleContainer.classList.add("title", "grid-cs-2", "grid-ce-3", "grid-rs-1", "grid-re-2")
 
         let title = document.createElement("p");
         title.textContent = this.title;
         titleContainer.appendChild(title);
 
         let artistContainer = document.createElement("div");
-        artistContainer.classList.add(["artist", "grid-cs-2", "grid-ce-3", "grid-rs-2", "grid-re-3"])
+        artistContainer.classList.add("artist", "grid-cs-2", "grid-ce-3", "grid-rs-2", "grid-re-3")
 
         let artist = document.createElement("p");
         artist.textContent = this.artist;
         artistContainer.appendChild(artist);
 
 
-        mainContainer.append(image);
-        mainContainer.append(title);
-        mainContainer.append(artist);
+        mainContainer.append(imageContainer);
+        mainContainer.append(titleContainer);
+        mainContainer.append(artistContainer);
+        mainContainer.addEventListener("click",this.onClick,false)
 
         fragment.appendChild(mainContainer);
 
         return fragment;
 
+      }
+
+      onClick(event) {
+        let previewImage = document.querySelector("div#preview>img");
+        let previewTitle = document.querySelector("div#title>p");
+        let previewArtist =  document.querySelector("div#artist>p");
+        preview.src=this.querySelector(".icon > img").src;
+        
       }
 
     }
